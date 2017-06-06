2bc693df54f59f6afffb35c04f9ccbff0e520bec
Suz Campbell <suz.blandon@gmail.com>
2017-06-06 11:32:34 -0600

    gulp autoprefixer, watch, etc.

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require("gulp");
+var watch = require("gulp-watch");
+var batch = require("gulp-batch");
+gulp.task("watch", function() {
+	watch(
+		"*.css",
+		batch(function(events, done) {
+			gulp.start("autoprefixer", done);
+		})
+	);
+});
+
+gulp.task("autoprefixer", function() {
+	var postcss = require("gulp-postcss");
+	var sourcemaps = require("gulp-sourcemaps");
+	var autoprefixer = require("autoprefixer");
+
+	return gulp
+		.src("style.css")
+		.pipe(sourcemaps.init())
+		.pipe(postcss([autoprefixer()]))
+		.pipe(sourcemaps.write("."))
+		.pipe(gulp.dest("./dist"));
+});
+gulp.task("default", function() {
+	// place code for your default task here
+});
